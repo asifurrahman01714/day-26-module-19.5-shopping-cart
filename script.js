72cafb319268a34314bf39ed353a01f8623afd6a
asifurrahman01714 <asif.eee.cu@gmail.com>
2021-01-27 13:09:49 +0600

    all function added

--- script.js
+++ script.js
@@ -1,54 +1,109 @@
 //plus btn click handler
 const plusBtn = document.getElementById('plusBtn');
-plusBtn.addEventListener('click',function () {
-
+plusBtn.addEventListener('click', function () {
     //capturing first input number
-    const firstInput = parseFloat(document.getElementById('firstInput').value);
-    const totalFirstInputNumber = firstInput + 1;
-    document.getElementById('firstInput').value = totalFirstInputNumber;
-  
-
+    captureInput('firstInput');
     //capturing price amount
-
-    const firstPrice = parseFloat(document.getElementById('firstPrice').innerText);
-    const totalFirstPrice = firstPrice + 1219;
-    document.getElementById('firstPrice').innerText = totalFirstPrice;
-
+    priceAmount('firstPrice', 1219);
     //capturing subtotal
-    const subTotal = parseFloat(document.getElementById('subTotal').innerText);
-    const totalSubTotal = subTotal + 1219;
-    document.getElementById('subTotal').innerText= totalSubTotal;
-
+    plusSubTotal(1219);
     //capturing total amount
-    const total = parseFloat(document.getElementById('total').innerText);
-    const totalFinal = totalSubTotal;
-    document.getElementById('total').innerText = totalSubTotal;
-    
-   
+    plusTotal(1219);
+
 })
 
+//case plus btn handler
+const casePlusBtn = document.getElementById('casePlusBtn');
+casePlusBtn.addEventListener('click', function () {
+    //capturing first input number
+    captureInput('caseInput');
+    //capturing price amount
+    priceAmount('caseFirstPrice', 59);
+    //capturing subtotal
+    plusSubTotal(59);
+    //capturing total amount
+    plusTotal(59);
+})
+
+// all plus button handler
+function captureInput(id) {
+    const firstInput = parseFloat(document.getElementById(id).value);
+    const totalFirstInputNumber = firstInput + 1;
+    document.getElementById(id).value = totalFirstInputNumber;
+
+}
+
+function priceAmount(id, price) {
+    const firstPrice = parseFloat(document.getElementById(id).innerText);
+    const totalFirstPrice = firstPrice + price;
+    document.getElementById(id).innerText = totalFirstPrice;
+}
+
+function plusSubTotal( price) {
+    const subTotal = parseFloat(document.getElementById('subTotal').innerText);
+    const totalSubTotal = subTotal + price;
+    document.getElementById('subTotal').innerText = totalSubTotal;
+    return totalSubTotal;
+    
+}
+
+function plusTotal(price) {
+    const total = parseFloat(document.getElementById('total').innerText);
+    const totalFinal = plusSubTotal(price);
+    document.getElementById('total').innerText = totalFinal;
+}
+// all plus button handler
 
 //minus btn click handler
 const minusBtn = document.getElementById('minusBtn');
-minusBtn.addEventListener('click',function () {
-
+minusBtn.addEventListener('click', function () {
     //capturing first input number
-    const firstInput = parseFloat(document.getElementById('firstInput').value);
-    const totalFirstInputNumber = firstInput - 1;
-    document.getElementById('firstInput').value = totalFirstInputNumber;
-  
-
+    minusInput('firstInput');
     //capturing price amount
-
-    const firstPrice = parseFloat(document.getElementById('firstPrice').innerText);
-    const totalFirstPrice = firstPrice - 1219;
-    document.getElementById('firstPrice').innerText = totalFirstPrice;
-   
-
+    minusFirstPrice('firstPrice',1219);
     //capturing subtotal
-    const subTotal = parseFloat(document.getElementById('subTotal').innerText);
-    const totalSubTotal = subTotal - 1219;
-    document.getElementById('subTotal').innerText= totalSubTotal;
+    minusSubTotal(1219);
+    //capturing total amount
+    minusTotal(1219);
 })
 
+//case minus button
+const caseMinusBtn = document.getElementById('caseMinusBtn');
+caseMinusBtn.addEventListener('click', function () {
+    //capturing first input number
+    minusInput('caseInput');
+    //capturing price amount
+    minusFirstPrice('caseFirstPrice',59);
+    //capturing subtotal
+    minusSubTotal(59);
 
+    //capturing total amount
+    minusTotal(59);
+});
+
+
+function minusInput(id) {
+    const firstInput = parseFloat(document.getElementById(id).value);
+    const totalFirstInputNumber = firstInput - 1;
+    document.getElementById(id).value = totalFirstInputNumber;
+
+}
+
+function minusFirstPrice(id, price) {
+    const firstPrice = parseFloat(document.getElementById(id).innerText);
+    const totalFirstPrice = firstPrice - price;
+    document.getElementById(id).innerText = totalFirstPrice;
+}
+
+function minusSubTotal(price) {
+    const subTotal = parseFloat(document.getElementById('subTotal').innerText);
+    const totalSubTotal = subTotal - price;
+    document.getElementById('subTotal').innerText = totalSubTotal;
+    return totalSubTotal;
+}
+
+function minusTotal(price) {
+    const total = parseFloat(document.getElementById('total').innerText);
+    const totalFinal = minusSubTotal(price);
+    document.getElementById('total').innerText = totalFinal;
+}
